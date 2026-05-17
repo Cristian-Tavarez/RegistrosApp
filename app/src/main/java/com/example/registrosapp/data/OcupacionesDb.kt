3fb31b356aed04af074471a41ace6f23790bb1ec
Cristian-Tavarez <cristian22041@gmail.com>
2026-05-17 06:19:47 -0700

    Modifica OcupacionesDb.kt

--- app/src/main/java/com/example/registrosapp/data/OcupacionesDb.kt
+++ app/src/main/java/com/example/registrosapp/data/OcupacionesDb.kt
@@ -3,7 +3,11 @@ package com.example.registrosapp.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [OcupacionEntity::class], version = 1)
+@Database(
+    entities = [OcupacionEntity::class, EmpleadoEntity::class],
+    version = 2
+)
 abstract class OcupacionesDb : RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
+    abstract fun empleadoDao(): EmpleadoDao
 }
