c7e199f207abe9e38eb4110080e166f85ffa27fa
Cristian-Tavarez <cristian22041@gmail.com>
2026-05-28 08:59:16 -0700

    Actualiza el EmpleadoEntity.kt

--- app/src/main/java/com/example/registrosapp/data/local/entity/EmpleadoEntity.kt
+++ app/src/main/java/com/example/registrosapp/data/local/entity/EmpleadoEntity.kt
@@ -1,4 +1,4 @@
-package com.example.registrosapp.data
+package com.example.registrosapp.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -10,5 +10,6 @@ data class EmpleadoEntity(
     val FechaIngreso: String,
     val Nombres: String,
     val Sexo: String,
+    val Ocupacion: String,
     val Sueldo: Double
 )
